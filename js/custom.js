f814598f05da078961d990a13898613d0ce3c20d
LiTO773 <luispinto2000@gmail.com>
2017-04-03 19:12:13 +0100

    Texts and 'Sobre Nós' page were updated. Added Typed.js and wink_emoji.svg. A few fonts were removed.

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// Configurações do ScrollFire
+let options = [
+  {selector: '#section_about', offset: 200, callback: typeCoderDojoDescription}
+]
+
+Materialize.scrollFire(options)
+
+function typeCoderDojoDescription () {
+  Typed.new('.small_description', {
+      strings: ['O CoderDojo é um movimento global que pretende, de forma gratuita e voluntária, ajudar crianças e jovens entre os 7 a 17 anos a apreender a programar.'],
+      typeSpeed: 0
+    })
+}
