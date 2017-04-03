e172fdccd8193201cb823b1110278d3404bbfb64
LiTO773 <luispinto2000@gmail.com>
2017-04-03 22:56:09 +0100

    Added scroll, fixed a few typos

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,13 @@ Materialize.scrollFire(options)
 
 function typeCoderDojoDescription () {
   Typed.new('.small_description', {
-      strings: ['O CoderDojo é um movimento global que pretende, de forma gratuita e voluntária, ajudar crianças e jovens entre os 7 a 17 anos a apreender a programar.'],
+      strings: ['O CoderDojo é um movimento global que pretende, de forma gratuita e voluntária, ajudar crianças e jovens entre os 7 a 17 anos a aprender a programar.'],
       typeSpeed: 0
     })
+}
+
+function scrollTo (id) {
+  $('html, body').animate({
+    scrollTop: $('#' + id).offset().top
+  }, 1200)
 }
