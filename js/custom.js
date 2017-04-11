55c49d4bf8a58ff0493805198fadebf68bc27789
LiTO773 <luispinto2000@gmail.com>
2017-04-11 11:47:21 +0100

    Testes com Bulma.css

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,9 @@
-// Configurações do ScrollFire
-let options = [
-  {selector: '#section_about', offset: 200, callback: typeCoderDojoDescription}
-]
-
-Materialize.scrollFire(options)
-
-function typeCoderDojoDescription () {
-  Typed.new('.small_description', {
-      strings: ['O CoderDojo é um movimento global que pretende, de forma gratuita e voluntária, ajudar crianças e jovens entre os 7 a 17 anos a aprender a programar.'],
-      typeSpeed: 0
-    })
-}
-
 function scrollTo (id) {
   $('html, body').animate({
     scrollTop: $('#' + id).offset().top
   }, 1200)
+  console.log('decibel');
+  if (id === 'about' && !hasBeenTyped) {
+    typeCoderDojoDescription()
+  }
 }
