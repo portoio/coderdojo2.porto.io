136e57f3656ef2b4bf1f0a8b6f34233faee2ff4e
LiTO773 <luispinto2000@gmail.com>
2017-04-11 21:18:21 +0100

    Finished 'Localização'; Added 'Contactos'; Minor tweaks

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 function scrollTo (id) {
   $('html, body').animate({
     scrollTop: $('#' + id).offset().top
-  }, 1200)
+  }, 500)
   console.log('decibel');
   if (id === 'about' && !hasBeenTyped) {
     typeCoderDojoDescription()
