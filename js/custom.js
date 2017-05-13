06964b8b390ea3f371f965e230f2e21d5f3904b6
LiTO773 <luispinto2000@gmail.com>
2017-05-13 12:31:54 +0100

    Updated icon fonts; Few changes

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,15 @@
-function scrollTo (id) {
-  $('html, body').animate({
-    scrollTop: $('#' + id).offset().top
-  }, 500)
-  console.log('decibel');
-  if (id === 'about' && !hasBeenTyped) {
-    typeCoderDojoDescription()
+$('document').ready(function() {
+  if (window.location.hash === "") { // Caso não exista um hash no url
+    $('#index_nav').removeClass('sidebar_color').addClass('current_section')
+  } else { // Caso contrário
+    $(window.location.hash + '_nav').removeClass('sidebar_color').addClass('current_section')
   }
+})
+
+function scrollTo (id) {
+  $(window.location.hash + '_nav').removeClass('current_section').addClass('sidebar_color') // Muda a cor do icone da secção anterior para azul
+  $('html, body').animate({
+    scrollTop: $('#' + id).offset().top // Dá scroll até à nova secção
+  }, 500)
+  $('#' + id + '_nav').removeClass('sidebar_color').addClass('current_section') // Muda a cor do icone da nova secção
 }
