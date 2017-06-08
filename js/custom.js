4227383244b781b6bfb91ae03cb425f59c413320
Francisco Fernandes <francisco.escola1@live.com.pt>
2017-06-08 17:16:14 +0100

    Google Map tweaks

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,17 @@ $('document').ready(function() {
   if (window.location.hash === '') { // Caso não exista um hash no url
     window.location.hash = '#index'
   }
-  $(window.location.hash + '_nav').removeClass('sidebar_color').addClass('current_section')
-})
+  $(window.location.hash + '_nav').removeClass('sidebar_color').addClass('current_section');
+	
+  $('#map').click(function(){
+	  $("#googleMaps").css("pointer-events", "auto");
+  });
+	
+  $('#map').mouseleave(function(){
+	  $("#googleMaps").css("pointer-events", "none");
+  });
+  
+});
 
 function scrollTo (id) {
   $(window.location.hash + '_nav').removeClass('current_section').addClass('sidebar_color') // Muda a cor do icone da secção anterior para azul
